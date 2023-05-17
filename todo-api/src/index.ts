12d6ed7a0be9da3faa74b974aedc9f157b0e12dd
Bash-Clevin <clevinbash@gmail.com>
2023-05-17 18:47:38 +0300

    feat: Test Routes

--- todo-api/src/index.ts
+++ todo-api/src/index.ts
@@ -1,7 +1,9 @@
 import Express from "express";
+import router from "./routes/routes.js";
 const app = Express();
 
 const port = 3000;
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Todo API server started on port ${port}`);
