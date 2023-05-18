04a2fe303b0199e2561c385968de5052ed93870d
Bash-Clevin <clevinbash@gmail.com>
2023-05-18 07:07:38 +0300

    feat: Add body parser

--- todo-api/src/index.ts
+++ todo-api/src/index.ts
@@ -1,8 +1,11 @@
 import Express from "express";
+import bodyParser from "body-parser";
 import router from "./routes/routes.js";
 const app = Express();
 
 const port = 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(port, () => {
