51be32de20b6e0df97173614aaf7713f007c3654
Bash-Clevin <clevinbash@gmail.com>
2023-05-18 07:08:45 +0300

    Test reddis connection

--- todo-api/src/routes/routes.ts
+++ todo-api/src/routes/routes.ts
@@ -1,10 +1,32 @@
 import Express from "express";
+import redisClient from "../lib/createRedisClient.js";
 const app = Express();
 const router = Express.Router();
 
-router.route("/api/v1/todos").get(async (req, res) => {
+interface todos {
+  key: string;
+  title: string;
+}
+
+router.route("/api/v1/todos").post(async (req, res) => {
+  const todoTitle: todos = {
+    key: req.body.key,
+    title: req.body.title,
+  };
   console.log("Called Get api/v1/todos");
-  res.setHeader("Content-Type", "application/json");
+  // res.setHeader("Content-Type", "application/json");
+
+  try {
+    await redisClient.set(todoTitle.key, todoTitle.title);
+    console.log(`Added Todo: [${todoTitle.title}] to cache`);
+    res.status(201).send(req.body);
+  } catch (error) {
+    console.log("Could not cache to redis", error);
+  }
 });
+
+router.route("/api/v1/todos").post(async (req, res) => {});
+
+router.route("/api/v1/search").post(async (req, res) => {});
 
 export default router;
