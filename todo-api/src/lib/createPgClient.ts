0f4cc49d6689232d1f7f9d0c270b6924bd5c5c66
Bash-Clevin <clevinbash@gmail.com>
2023-05-25 00:44:10 +0300

    feat: use connectionstring

--- todo-api/src/lib/createPgClient.ts
+++ todo-api/src/lib/createPgClient.ts
@@ -1,21 +1,10 @@
 import pg from "pg";
 import env from "./env.js";
 
-const { Client } = pg;
+const { Pool } = pg;
 
-const postgresClient = new Client({
-  user: env.postgresUser,
-  host: env.postgresHost,
-  port: Number(env.postgresPort),
-  database: env.postgresDatabase,
-  password: env.postgresPassword,
+const postgresPool = new Pool({
+  connectionString: env.postgresDbUrl,
 });
 
-try {
-  postgresClient.connect();
-  console.log("Connected to PostgreSQL");
-} catch (error) {
-  console.error("Failed to connect to PostgreSQL", error);
-}
-
-export default postgresClient;
+export default postgresPool;
