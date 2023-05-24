86cc7bdf97ea8b5852fd2289bb2a01f8c8f7f333
Bash-Clevin <clevinbash@gmail.com>
2023-05-25 00:51:09 +0300

    feat:update cors

--- todo-api/src/index.ts
+++ todo-api/src/index.ts
@@ -2,16 +2,12 @@ import Express from "express";
 import bodyParser from "body-parser";
 import router from "./routes/routes.js";
 import cors from "cors";
-import env from "./lib/env.js";
 
 const app = Express();
 
 const port = 4000;
-const corsOptions = {
-  origin: `http://${env.todoClientHost}:${env.todoClientPORT}`,
-  optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
