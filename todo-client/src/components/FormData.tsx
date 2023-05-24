6d502a93229e07582098a272ac84ae89fc6a6419
Bash-Clevin <clevinbash@gmail.com>
2023-05-25 00:39:39 +0300

    feat: use async

--- todo-client/src/components/FormData.tsx
+++ todo-client/src/components/FormData.tsx
@@ -1,36 +1,30 @@
+import axios from 'axios'
 import React, { ChangeEvent, FormEvent, useState } from 'react'
-
 function FormData(): React.JSX.Element {
-  const [data, setData] = useState({value: ''})
+
+  const [data, setData] = useState('')
   
   const handleDataChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setData({value: event.target.value})
+    setData(event.target.value)
   }
 
-  const handleDataSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const handleDataSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    fetch('http://localhost:4000/api/v1/todos',{ 
-      method: 'POST', 
-      headers: {
-        "Content-Type": "application/json"
-      }, 
-      body: JSON.stringify(data)
+    await axios.post('/api/v1/todos', {
+      data
     })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-    })
-    .catch(error => {
-      console.error(error)
-    })
+    setData('')  
   }
 
   return (
+    <div>
+   
     <form onSubmit={handleDataSubmit}>
-      <input type="text" name="todo" value={data.value} onChange={handleDataChange} />
+      <input type="text" name="todo" value={data} onChange={handleDataChange} />
       <button type='submit'>Add todo</button>
-    </form>
+    </form> 
+    </div>    
   )
 }
 
