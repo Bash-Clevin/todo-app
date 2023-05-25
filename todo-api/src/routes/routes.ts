5a6797a496fd9f62aac88c352d718ab7076320b4
Bash-Clevin <clevinbash@gmail.com>
2023-05-25 10:49:41 +0300

    feat: Add get logic

--- todo-api/src/routes/routes.ts
+++ todo-api/src/routes/routes.ts
@@ -1,6 +1,8 @@
 import Express from "express";
 import { todos } from "../interfaces/todos.js";
 import { insertToPostgres, insertToRedis } from "../lib/insertData.js";
+import redisClient from "../lib/createRedisClient.js";
+import postgresPool from "../lib/createPgClient.js";
 const app = Express();
 const router = Express.Router();
 const searchIndexName = "todos";
@@ -9,7 +11,7 @@ const searchIndexType = "todo";
 router.route("/api/v1/todos").post(async (req, res) => {
   const { data } = req.body;
   const todoTitle: todos = {
-    key: "todo",
+    key: "todos",
     title: data,
   };
 
@@ -24,7 +26,26 @@ router.route("/api/v1/todos").post(async (req, res) => {
   res.status(201).send(req.body);
 });
 
-router.route("/api/v1/todos").get(async (req, res) => {});
+router.route("/api/v1/todos").get(async (req, res) => {
+  let todos: any[] = [];
+  const cachedTodoSet = await redisClient.sMembers("todos");
+  if (cachedTodoSet == null) {
+    postgresPool.query("SELECT title FROM todo", (error, todoRows) => {
+      if (error) {
+        throw error;
+      }
+      todos = todoRows.rows;
+      console.log("todos from Postgres", todos);
+      res.send(todos);
+    });
+  } else {
+    cachedTodoSet.forEach((member) => {
+      todos.push(member);
+    });
+    console.log("Got todos from Redis cache");
+    res.send(todos);
+  }
+});
 
 router.route("/api/v1/search").post(async (req, res) => {});
 
